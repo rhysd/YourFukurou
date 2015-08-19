025f6de93f29fef18cd123fb5b7708a5389fd601
rhysd <lin90162@yahoo.co.jp>
2015-08-19 17:48:59 +0900

    make tweet UI using Semantic UI

--- plugin/twitter/src/sink.jsx
+++ plugin/twitter/src/sink.jsx
@@ -5,12 +5,59 @@ class Tweet extends React.Component {
         super(props);
     }
 
-    render() {
+    build_tweet_component(t) {
         return (
-            <div className="tweet">
-                {this.props.raw_json}
+            <div className="ui comments tweet">
+                <div className="comment">
+                    <a className="avatar" href={"https://twitter.com/" + t.user.screen_name}>
+                        <img src={t.user.profile_image_url} />
+                    </a>
+                    <div className="content">
+                        <a className="author" href={"https://twitter.com/" + t.user.screen_name}>
+                            @{t.user.screen_name}
+                        </a>
+                        <div className="metadata">
+                            <span className="date created-at">{t.created_at}</span>
+                        </div>
+                        <div className="text">
+                            {t.text}
+                        </div>
+                    </div>
+                </div>
             </div>
         );
+    }
+
+    build_retweet_component(t, user) {
+        return (
+            <div className="ui comments tweet">
+                <div className="comment">
+                    <a className="avatar" href={"https://twitter.com/" + t.user.screen_name}>
+                        <img src={t.user.profile_image_url} />
+                    </a>
+                    <div className="content">
+                        <a className="author" href={"https://twitter.com/" + t.user.screen_name}>
+                            @{t.user.screen_name}
+                        </a>
+                        <div className="metadata">
+                            <span className="retweeted-by"><i className="fa fa-retweet"></i> Retweeted by @{user.screen_name}</span>
+                            <span className="date created-at">{t.created_at}</span>
+                        </div>
+                        <div className="text">
+                            {t.text}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        if ('retweeted_status' in this.props.tweet) {
+            return this.build_retweet_component(this.props.tweet.retweeted_status, this.props.tweet.user);
+        } else {
+            return this.build_tweet_component(this.props.tweet);
+        }
     }
 }
 
@@ -21,6 +68,7 @@ StreamApp.registerSink({
     source: "twitter",
 
     stylesheets: [
+        "style.css"
     ],
 
     initialize: function() {
@@ -30,7 +78,7 @@ StreamApp.registerSink({
 
     streams: {
         tweet: function(json) {
-            return <Tweet raw_json={JSON.stringify(json, null, 2)} />;
+            return <Tweet tweet={json} />;
         }
     }
 });
--- renderer/feed.jsx
+++ renderer/feed.jsx
@@ -13,7 +13,8 @@ export default class Feed extends React.Component {
                 for (const stream_name in streams) {
                     console.log('Feed: registered: ' + source_name + '-' + stream_name);
                     this.props.router.registerRenderer(source_name, stream_name, new_item => {
-                        const new_children = [new_item].concat(this.state.children);
+                        const added = this.state.children.length !== 0 ? [new_item, <div className="ui divider"></div>] : [new_item];
+                        const new_children = added.concat(this.state.children);
                         this.setState({children: new_children});
                     });
                 }
