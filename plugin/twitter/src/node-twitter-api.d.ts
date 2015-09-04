2c112ae423fea2b4fe91255f6d6f43baff5a08c3
rhysd <lin90162@yahoo.co.jp>
2015-09-05 00:43:37 +0900

    add missing type definitions for node-twitter-api

---
+++ plugin/twitter/src/node-twitter-api.d.ts
@@ -0,0 +1,19 @@
+declare module TwitterAPI {
+    interface OAuthInfo {
+        consumerKey: string;
+        consumerSecret: string;
+        callback: string;
+    }
+
+    export class Authenticator {
+        constructor(tokens: OAuthInfo);
+        getRequestToken(callback: (err: Error, requestToken: string, requestTokenSecret: string) => void): any;
+        getAuthUrl(request_token: string): string;
+        getAccessToken(request_token: string, request_token_secret: string, verifier: string, callback: (error: Error, access_token: string, access_token_secret: string) => void): void;
+    }
+}
+
+declare module "node-twitter-api" {
+    var tw: typeof TwitterAPI.Authenticator;
+    export = tw;
+}
