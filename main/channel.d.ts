abbef8c2c7a1a1d3b7b160e561133af1aab5dfa6
rhysd <lin90162@yahoo.co.jp>
2016-04-08 09:03:09 +0900

    add ipc sender

---
+++ main/channel.d.ts
@@ -0,0 +1,1 @@
+type Channel = 'yf:tweet';
---
+++ main/ipc_sender.ts
@@ -0,0 +1,12 @@
+import {BrowserWindow} from 'electron';
+
+export default class IpcSender {
+    constructor(
+        public sender: Electron.WebContents = BrowserWindow.getFocusedWindow().webContents
+    ) {
+    }
+
+    send(channel: Channel, ...args: any[]) {
+        this.sender.send(channel, ...args);
+    }
+}
