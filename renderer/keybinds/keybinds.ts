8a93840bc75fa2a948f52e573f562216d0fd9c88
rhysd <lin90162@yahoo.co.jp>
2016-04-21 12:14:37 +0900

    preparse key sequence string

--- renderer/keybinds/keybinds.ts
+++ renderer/keybinds/keybinds.ts
@@ -8,6 +8,13 @@ export interface KeyEvent {
     altKey: boolean;
 }
 
+interface PreparsedSeq {
+    char: string;
+    ctrlKey: boolean;
+    metaKey: boolean;
+    altKey: boolean;
+}
+
 const ReCharFromSeq = /[^+]+$/;
 const SpecialChars = {
     Enter: 'enter',
@@ -26,20 +33,60 @@ function getCharFrom(e: KeyEvent) {
 
 export default class KeyBinds<ActionType> {
     enabled: boolean;
+    keymap: I.Map<PreparsedSeq, ActionType>;
 
     constructor(
-        public keymap: I.Map<string, ActionType>,
+        default_map: I.Map<string, ActionType>,
         public handlers: I.Map<ActionType, () => void>
     ) {
         this.enabled = true;
+        this.keymap = I.Map<PreparsedSeq, ActionType>(
+            default_map
+            .entrySeq()
+            .map(
+                (kv: [string, ActionType]) =>
+                    [this.parseSequence(kv[0]), kv[1]]
+            ).filter(kv => kv[0] !== null)
+        );
     }
 
     registerSequence(seq: string, action: ActionType) {
-        this.keymap = this.keymap.set(seq, action);
+        const preparsed = this.parseSequence(seq);
+        if (preparsed === null) {
+            return false;
+        }
+        this.keymap = this.keymap.set(preparsed, action);
+        return true;
     }
 
     registerHandler(action: ActionType, handler: () => void) {
         this.handlers = this.handlers.set(action, handler);
+    }
+
+    parseSequence(seq: string): PreparsedSeq {
+        const m = seq.match(ReCharFromSeq);
+        if (m === null) {
+            return null;
+        }
+
+        let e: PreparsedSeq = {
+            char: m[0],
+            ctrlKey: false,
+            metaKey: false,
+            altKey: false,
+        };
+
+        if (seq.contains('ctrl+')) {
+            e.ctrlKey = true;
+        }
+        if (seq.contains('alt+')) {
+            e.altKey = true;
+        }
+        if (seq.contains('cmd+')) {
+            e.metaKey = true;
+        }
+
+        return e;
     }
 
     resolveEvent(key: KeyEvent): ActionType {
@@ -47,31 +94,22 @@ export default class KeyBinds<ActionType> {
             return null;
         }
 
-        let keys = I.Iterable(this.keymap.keySeq());
+        const filtered = this.keymap
+                .keySeq()
+                .filter(p => {
+                    if ((key.ctrlKey !== p.ctrlKey) ||
+                        (key.metaKey !== p.metaKey) ||
+                        (key.altKey !== p.altKey)) {
+                        return false;
+                    }
+                    return getCharFrom(key) === p.char;
+                });
 
-        if (key.ctrlKey) {
-            keys = keys.filter(seq => seq.contains('ctrl+'));
-        }
-        if (key.altKey) {
-            keys = keys.filter(seq => seq.contains('alt+'));
-        }
-        if (key.metaKey) {
-            keys = keys.filter(seq => seq.contains('cmd+'));
-        }
-
-        keys = keys.filter(seq => {
-            const m = seq.match(ReCharFromSeq);
-            if (m === null) {
-                return false;
-            }
-            return getCharFrom(key) === m[0];
-        });
-
-        if (keys.isEmpty()) {
+        if (filtered.isEmpty()) {
             return null;
         }
 
-        return this.keymap.get(keys.first());
+        return this.keymap.get(filtered.first());
     }
 
     handleAction(action: ActionType) {
