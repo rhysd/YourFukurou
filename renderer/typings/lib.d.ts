d88a3f0a3868219305c0982b444dfe1c5b9c1249
rhysd <lin90162@yahoo.co.jp>
2016-05-18 16:11:51 +0900

    update dependencies

--- renderer/typings/lib.d.ts
+++ renderer/typings/lib.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/main.d.ts" />
+/// <reference path="../../typings/index.d.ts" />
 /// <reference path="../../node_modules/immutable/dist/immutable.d.ts" />
 /// <reference path="../../node_modules/dexie/dist/dexie.d.ts" />
 
