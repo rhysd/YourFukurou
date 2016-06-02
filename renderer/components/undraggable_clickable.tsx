fa68827d1c5e960d3ac5901fcb626e2323cf6892
rhysd <lin90162@yahoo.co.jp>
2016-06-03 00:09:19 +0900

    do not use function literal for props

--- renderer/components/undraggable_clickable.tsx
+++ renderer/components/undraggable_clickable.tsx
@@ -1,18 +1,45 @@
 import * as React from 'react';
 
-interface UndraggableClickableProps extends React.Props<any> {
+interface Props extends React.Props<any> {
     className?: string;
     onClick: (e: React.MouseEvent) => void;
 }
 
-const UndraggableClickable: React.StatelessComponent<UndraggableClickableProps> = props => (
-    <div
-        className={props.className}
-        onMouseDown={() => { this.dragged = false; }}
-        onMouseMove={() => { this.dragged = true; }}
-        onMouseUp={e => !this.dragged && props.onClick(e) }
-    >
-        {props.children}
-    </div>
-);
-export default UndraggableClickable;
+export default class UndraggableClickable extends React.Component<Props, {}> {
+    dragged: boolean;
+
+    constructor(props: Props) {
+        super(props);
+        this.onMouseDown = this.onMouseDown.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onMouseDown() {
+        this.dragged = false;
+    }
+
+    onMouseMove() {
+        this.dragged = true;
+    }
+
+    onClick(e: React.MouseEvent) {
+        if (!this.dragged) {
+            this.props.onClick(e);
+        }
+    }
+
+    render() {
+        return (
+            <div
+                className={this.props.className}
+                onMouseDown={this.onMouseDown}
+                onMouseMove={this.onMouseMove}
+                onClick={this.onClick}
+            >
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
